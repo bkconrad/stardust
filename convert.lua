d078ea03838f3558b561d7c2af28e95bf2aea87b
Bryan Conrad <conrad.bryan.k@gmail.com>
2013-05-08 14:27:13 -0400

    add convert

---
+++ convert.lua
@@ -0,0 +1,36 @@
+-- Convert Objects
+-- Converts selected polygonal objects in to the specified type, keeping the
+-- same geometry
+--
+-- This work is released into the public domain
+-- Authored by kaen
+
+function getArgsMenu()
+
+	menu = 	{
+		ToggleMenuItem.new("Max Distance", {"WallItem", "PolyWall", "Zone", "LoadoutZone", "GoalZone", "SlipZone"}, 1, "Object type to convert to")
+	}
+
+	return "Covert Objects", menu
+end
+
+-- map of object types to default arguments
+OBJECT_MAP = {
+}
+
+function main()
+	local objectType = table.remove(arg, 1)
+
+	local objects = plugin:getSelectedObjects()
+
+	for _, obj in pairs(objects) do
+		local geom = obj:getGeom()
+		if type(geom) == "table" then
+			local constructor = loadstring("return " .. objectType .. ".new")()
+			local newObj = constructor()
+			newObj:setGeom(geom)
+			plugin:addItem(newObj)
+		end
+	end
+end   
+
