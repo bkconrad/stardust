0c81203fcac82f68cf08fdd93eaf3447e30275e9
Bryan Conrad <conrad.bryan.k@gmail.com>
2013-08-09 12:12:20 -0700

    fix up convert.lua and add basic support for point types

--- convert.lua
+++ convert.lua
@@ -12,13 +12,37 @@ IMPLICITLY_CLOSED_CLASS_IDS = {
 	[ObjType.Zone] = true
 }
 
+TARGET_TYPES = {
+  "WallItem",
+  "PolyWall",
+  "Zone",
+  "LoadoutZone",
+  "GoalZone",
+  "LineItem",
+  "FlagItem",
+  "ResourceItem",
+  "TestItem",
+  "Mine",
+  "SpyBug",
+  "SoccerBallItem",
+  "Spawn",
+  "RepairItem",
+  "EnergyItem",
+  "SpeedZone",
+  "Turret",
+  "ForceFieldProjector",
+  "Teleporter",
+  "Asteroid",
+}
+
 function getArgsMenu()
 
 	menu = 	{
-		ToggleMenuItem.new("Max Distance", {"WallItem", "PolyWall", "Zone", "LoadoutZone", "GoalZone", "LineItem"}, 1, "Object type to convert to")
+		ToggleMenuItem.new("New Object Type", TARGET_TYPES, 1, "Object type to convert to"),
+		YesNoMenuItem.new("Delete Old Objects", 2, "Delete the original objects after the new ones are created")
 	}
 
-	return "Covert Objects", menu
+	return "Convert Objects", menu
 end
 
 -- map of object types to default arguments
@@ -26,32 +50,37 @@ OBJECT_MAP = {
 }
 
 function main()
-	local objectType = table.remove(arg, 1)
+  local objectType = table.remove(arg, 1)
+  local deleteOld  = table.remove(arg, 1)
 
-	local objects = plugin:getSelectedObjects()
+  local objects = plugin:getSelectedObjects()
 
-	for _, obj in pairs(objects) do
-		obj:setSelected(false)
-		local geom = obj:getGeom()
-		if type(geom) == "table" then
-			local constructor = loadstring("return " .. objectType .. ".new")()
-			local newObj = constructor()
+  for _, obj in pairs(objects) do
+    obj:setSelected(false)
+    local geom = obj:getGeom()
+    local constructor = loadstring("return " .. objectType .. ".new")()
+    local newObj = constructor()
 
-			-- add or remove closing point as needed
-			if IMPLICITLY_CLOSED_CLASS_IDS[obj:getClassId()] then
-				if not IMPLICITLY_CLOSED_CLASS_IDS[newObj:getClassId()] then
-					table.insert(geom, geom[1])
-				end
-			elseif geom[1] == geom[#geom] then
-				if IMPLICITLY_CLOSED_CLASS_IDS[newObj:getClassId()] then
-					table.remove(geom, #geom)
-				end
-			end
+    if type(geom) == "table" then
+      -- add or remove closing point as needed
+      if IMPLICITLY_CLOSED_CLASS_IDS[obj:getClassId()] then
+        if not IMPLICITLY_CLOSED_CLASS_IDS[newObj:getClassId()] then
+          table.insert(geom, geom[1])
+        end
+      elseif geom[1] == geom[#geom] then
+        if IMPLICITLY_CLOSED_CLASS_IDS[newObj:getClassId()] then
+          table.remove(geom, #geom)
+        end
+      end
+    end
 
-			newObj:setGeom(geom)
-			newObj:setSelected(true)
-			plugin:addItem(newObj)
-		end
-	end
+    newObj:setGeom(geom)
+    newObj:setSelected(true)
+    bf:addItem(newObj)
+
+    if deleteOld == "Yes" then
+      obj:removeFromGame()
+    end
+  end
 end   
 
