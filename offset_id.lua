94fd39f62d0d257754d65c860f4e8ba32877a9a6
Bryan Conrad <conrad.bryan.k@gmail.com>
2013-08-09 20:29:23 -0700

    add offset_id.lua

---
+++ offset_id.lua
@@ -0,0 +1,29 @@
+-- Offset ID
+--
+-- Add/subtract a number to the ID of all selected objects which have
+-- user-assigned IDs.
+--
+-- This work is released into the public domain
+-- Authored by kaen
+
+function getArgsMenu()
+	menu = 	{
+		CounterMenuItem.new("Offset",  0, 1, -0xFFFF, 0xFFFF, "", "", "The number to add to each ID"),
+	}
+	return "Offset ID", menu
+end
+
+function main()
+  local offset = table.remove(arg, 1)
+
+	local objects = plugin:getSelectedObjects()
+
+  for _, obj in ipairs(objects) do
+    local id = obj:getId()
+    if id >= 0 and id + offset >= 0 then
+      obj:setId(id + offset)
+    end
+  end
+
+end   
+
