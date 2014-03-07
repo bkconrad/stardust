b4856bdd758e5be3794e155be78e2c1cdef3d5c6
Bryan Conrad <bkconrad@gmail.com>
2014-03-07 14:25:51 -0800

    add fuzzy join

---
+++ editor_plugins/fuzzy_join.lua
@@ -0,0 +1,97 @@
+local sd = require('stardust')
+function getArgsMenu()
+	return "Fuzzy Join", "Join stubborn barriers", "Ctrl+Shift+J"
+end
+
+function main()
+	local objects = plugin:getSelectedObjects()
+	local joinOccured = true
+	objects = sd.keep(objects, WallItem)
+
+	while joinOccured do
+		joinOccured = false
+
+		for i=1,#objects do
+			local a = objects[i]:getGeom()
+
+			for j=i+1,#objects do
+				local b = objects[j]:getGeom()
+				local result = fuzzyJoin(a, b)
+
+				if i ~= j and result then
+					-- TODO: average width?
+					local newWallItem = WallItem.new(result, 50)
+					objects[i]:removeFromGame()
+					objects[j]:removeFromGame()
+					table.remove(objects, j)
+					table.remove(objects, i)
+					table.insert(objects, newWallItem)
+					bf:addItem(newWallItem)
+					joinOccured = true
+					break
+				end
+			end
+
+			if joinOccured then
+				break
+			end
+		end
+	end
+end
+
+function fuzzyJoin(a, b)
+	local delta = 2500.0
+	local result
+
+	-- check end point connections
+	for aIndex=1,#a,#a-1 do
+		for bIndex=1,#b,#b-1 do
+
+			print()
+			print(#b)
+			print(bIndex)
+
+			-- TODO: average join point
+			if point.distSquared(a[aIndex], b[bIndex]) <= delta then
+				local first, last
+				if aIndex == 1 and bIndex == 1 then
+					first, last = sd.reverse(a), b
+				elseif aIndex == 1 then
+					first, last = b, a
+				elseif bIndex == 1 then
+					first, last = a, b
+				else
+					first, last = a, sd.reverse(b)
+				end
+
+				local mid = midpoint(first[#first], last[1])
+
+				-- remove the connected points
+				table.remove(first, #first)
+				table.remove(last, 1)
+
+				return concat(first, mid, last)
+			end
+		end
+	end
+end
+
+function concat(...)
+	local result = {}
+
+	for i=1,#arg do
+		local t = arg[i]
+		if type(t) == 'table' then
+			for j=1,#t do
+				table.insert(result, t[j])
+			end
+		else
+			table.insert(result, t)
+		end
+	end
+	return result
+end
+
+function midpoint(a, b)
+	return a + (.5 * (b-a))
+end
