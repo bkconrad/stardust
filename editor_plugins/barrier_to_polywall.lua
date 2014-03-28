f90ea6c8a3112d80943467c2ac1bc9b4ba87bd0a
Bryan Conrad <bkconrad@gmail.com>
2014-03-28 15:24:25 -0700

    rudimentary B2P implementation

--- editor_plugins/barrier_to_polywall.lua
+++ editor_plugins/barrier_to_polywall.lua
@@ -22,104 +22,18 @@ function main()
 		-- get the barrier's skeleton
 		local geom = barrier:getGeom()
 		local width = barrier:getWidth() / 2
+		local segments = { }
 
-		-- make a pairs of tables to hold the result. one for each side
-		-- we'll fill the right side up front to back, and the left side up from
-		-- back to front. when we get to the end of just one pass, we'll be able
-		-- to string them end to end for our result.
-		local leftResult, rightResult = { }, { }
-
-		-- we'll also need to keep track of the angle of the last segment, for
-		-- bisecting the angles below
-		local lastAngle = nil
-
-		-- and also keep track of whether the last segment had a cutout on the
-		-- left or right side
-		local cutLeft, cutRight = false, false
-
-		-- for each segment (note that this is the number of points - 1)
 		for i = 1,#geom-1 do
-
-			-- get the points in this segment
-			p1, p2 = geom[i], geom[i+1]
-
-			-- find the angle of this segment
-			local thisAngle = math.atan2(p2.y - p1.y, p2.x - p1.x)
-
-			-- if there was a segment before this one
-			if lastAngle ~= nil then
-
-				-- bisect the angle (cut it in half)
-				local halfAngle = angularDistance(thisAngle + math.pi, lastAngle) / 2 + thisAngle
-				local normal = point.new(math.cos(halfAngle), math.sin(halfAngle))
-
-				-- extrude the segment's geometry
-				local segmentGeom = extrudeSegment(p1, p2, width, i == 1, i == #geom - 1)
-				local rightIntersections, rightSegments = findIntersections(p1, p1 - normal * width * width, segmentGeom)
-				local leftIntersections, leftSegments = findIntersections(p1, p1 + normal * width * width, segmentGeom)
-
-				-- handle cutouts
-				if cutRight == true then
-					table.insert(rightResult, segmentGeom[1])
-				end
-
-				if cutLeft == true then
-					table.insert(leftResult, segmentGeom[4])
-				end
-
-				-- if the "head" segment was hit, we'll add the corner points 
-				-- to make a v-like cut out for walls with sharp corners
-				for k,v in pairs(rightSegments) do
-					print(k,v)
-				end
-				if rightSegments[2] == 2 then
-					table.insert(rightResult, segmentGeom[2])
-					cutRight = true
-				end
-				table.insert(rightResult, rightIntersections[1])
-
-				if leftSegments[2] == 2 then
-					table.insert(leftResult, segmentGeom[3])
-					cutLeft = true
-				end
-				table.insert(leftResult, leftIntersections[1])
-
-				bf:addItem(LineItem.new(segmentGeom))
-				bf:addItem(LineItem.new(p1, p1 + normal * width))
-				bf:addItem(LineItem.new(p1, p1 - normal * width))
-
-				-- if #rightIntersections > 0 then
-				-- 	for i,p in ipairs(rightIntersections) do
-				-- 		print(p)
-				-- 		bf:addItem(Mine.new(p))
-				-- 	end
-				-- end
-
-				-- if #leftIntersections > 0 then
-				-- 	for i,p in ipairs(leftIntersections) do
-				-- 		print(p)
-				-- 		bf:addItem(Mine.new(p))
-				-- 	end
-				-- end
-
-			end
-
-			-- prepare for next segment
-			p1 = p2
-			lastAngle = thisAngle
-			cutLeft, cutRight = false, false
-
+			table.insert(segments, extrudeSegment(geom[i], geom[i+1], width, i == 1, i == #geom-1))
 		end
 
-		sd.append(rightResult, sd.reverse(leftResult))
-		bf:addItem(PolyWall.new(unpack(rightResult)))
+		results = Geom.clipPolygons(ClipType.Union, { }, segments)
+		for k,geom in ipairs(results) do
+			bf:addItem(PolyWall.new(geom))
+		end
 
 	end)
-end
-
--- Find the angular distance (between 0 and pi) of theta1 and theta2, in radians
-function angularDistance(theta1, theta2)
-	return math.fmod(math.fmod(theta2, math.tau) - math.fmod(theta1, math.tau), math.tau)
 end
 
 -- Gets the extrusion of the segment in the order { p1r, p2r, p2l, p1l }
@@ -150,66 +64,6 @@ function extrudeSegment(p1, p2, width, isFirst, isLast)
 		p1 - width * normal - .5 * width * unit * tailFactor
 	}
 
-end
-
--- find all intersections between 
-function findIntersections(p1, p2, geom)
-	local result = { }
-	local segments = { }
-	for i = 1,#geom do
-		local j = i + 1
-		if i == #geom then
-			j = 1
-		end
-
-		local intersectionPoint = intersection(p1, p2, geom[i], geom[j])
-		if intersectionPoint ~= nil then
-			table.insert(result, intersectionPoint)
-			table.insert(segments, i)
-		end
-	end
-
-	return result, segments
-end
-
-function cross(p, q)
-	return p.x * q.y - p.y * q.x
-end
-
-function intersection(p, p2, q, q2)
-	local r = p2 - p
-	local s = q2 - q
-	local t = cross(q - p, s) / cross(r, s)
-	local u = cross(q - p, r) / cross(r, s)
-
-	-- If r × s = 0 and (q − p) × r = 0, then the two lines are collinear. If in
-	-- addition, either 0 ≤ (q − p) · r ≤ r · r or 0 ≤ (p − q) · s ≤ s · s, then
-	-- the two lines are overlapping.
-	if cross(r,s) == 0 then
-		if cross(q-p, r) == 0 then
-			-- If r × s = 0 and (q − p) × r = 0, but neither 0 ≤ (q − p) · r ≤ r · r
-			-- nor 0 ≤ (p − q) · s ≤ s · s, then the two lines are collinear but
-			-- disjoint.
-			if not ((0 <= point.dot(q-p, r) and point.dot(q-p, r) < point.dot(r, r)) or (0 <= point.dot(p-q, s) and point.dot(p-q, s) < point.dot(s, s)) ) then
-				print('collinear')
-				return nil
-			end
-		else
-			-- If r × s = 0 and (q − p) × r ≠ 0, then the two lines are parallel and non-intersecting.
-			print('parallel')
-			return nil
-		end
-	end
-
-	-- If r × s ≠ 0 and 0 ≤ t ≤ 1 and 0 ≤ u ≤ 1, the two line segments meet at the point p + t r = q + u s.
-	if cross(r, s) ~= 0 and 0 <= t and t <= 1 and 0 <= u and u <= 1 then
-		return q + u * s
-	end
-
-	-- Otherwise, the two line segments are not parallel but do not intersect.
-	print('non intersecting')
-
-	return nil
 end
 
 function normalize(p)
